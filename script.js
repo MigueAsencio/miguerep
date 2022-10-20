0d8d7810964e595f7bdcce20f2e0f38bec5ec079
Miguel Asencio <miguel.asencio.epet20@gmail.com>
2022-10-20 20:22:53 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,21 @@
+//#region 
+const Nombre = 'Miguel';
+var Apellido = 'Apellido';
+let dni = '46257867';
+//#endregion
+
+//#region
+console.log(Nombre);
+console.log(Apellido);
+console.log(dni);
+//#endregion
+
+//#region
+function resta(n1, n2) {
+    let resultado = n1 - n2;
+    return resultado;
+}
+
+let n1 = 10;
+let n2 = 5;
+//#endregion
